a0fa56062ce1020e3272fe1cca07c0d1fa81ac08
Salim Mayan <mailsalim@gmail.com>
2021-07-11 10:47:00 -0700

    cleaned up version - still no way to pass base 64 string to Create POST controller

--- BacheloretteClient/Controllers/BachelorettesController.cs
+++ BacheloretteClient/Controllers/BachelorettesController.cs
@@ -19,115 +19,23 @@ namespace BacheloretteClient.Controllers
             // var allBachelorettes = Bachelorette.GetAll();
             return View();
         }
-        // [HttpPost]
-        // public IActionResult Index(Bachelorette bachelorette)
-        // {
-        //   Bachelorette.Post(bachelorette);
-        //   return RedirectToAction("Index");
-        // }
-        // public IActionResult Details(int id)
-        // {
-        //   var bachelorette = Bachelorette.GetDetails(id);
-        //   return View(bachelorette);
-        // }
-        // public IActionResult Edit(int id)
-        // {
-        //   var bachelorette = Bachelorette.GetDetails(id);
-        //   return View(bachelorette);
-        // }
-        // [HttpPost]
-        // public IActionResult Details(int id, Bachelorette bachelorette)
-        // {
-        //   bachelorette.BacheloretteId = id;
-        //   Bachelorette.Put(bachelorette);
-        //   return RedirectToAction("Details", id);
-        // }
-        // public IActionResult Delete(int id)
-        // {
-        //   Bachelorette.Delete(id);
-        //   return RedirectToAction("Index");
-        // }
         [HttpGet]
         public IActionResult Create()
         {
+            Console.WriteLine("Inside create GET");
             return View();
         }
         [HttpPost]
         public IActionResult Create(Bachelorette newBachelorette)
         {
             // Bachelorette.Post(newBachelorette);
+            Console.WriteLine("Inside create POST");
             Console.WriteLine("BacheloretteId is " + newBachelorette.BacheloretteId);
             Console.WriteLine("Description is " + newBachelorette.Description);
-            Console.WriteLine("Bachelor Id is " + newBachelorette.Base64);
-
-            // string script = string.Format("localStorage.userId= '{0}';", "12345");
-            // Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "key", script, true);
-            // var _isSupportLocalStorage = (('localStorage' in window) && (window['localStorage'] !== null));
-            // window.localStorage.getItem(b64StringID);
-            //       var storageItem = Request.Form["b64StringID"];
-            //        Console.WriteLine("storageItem is " + storageItem);
-            // var myStorage = window.localStorage;
-            // const cat = myStorage.getItem('b64StringID');
-            // result = ProtectedLocalStorage.Equals("b64StringID");
-  
-            // ScriptManager.RegisterStartupScript(this, GetType(), "displayalertmessage", "Showalert();", true);
-            // ScriptManager.RegisterClientScriptBlock(UpdatePanel1, this.GetType(), "script", "alert('Hi');", true);
-// inject ProtectedLocalStorage LocalStorage;
-
-            return RedirectToAction("Create");
-            // var ls = localStorage;
-            //       var custStr = ls.getItem("b64StringID");
-
-            // @inject ProtectedLocalStorage LocalStorage;
-            // <p>@TxtFromLocalStorage</p>
-
-
-            // private string TxtFromLocalStorage = string.Empty;
-            // private const string _localStorage = "b64StringID";
-
-            // protected override async Task OnInitializedAsync()
-            // {
-            //     var result = await LocalStorage.GetAsync<string>(_localStorage);
-            //     TxtFromLocalStorage = result.Success ? result.Value : "Can't read local storage";
-            // }
-        }
-
-// public class JsInteropClasses1
-// {
-//     private readonly IJSRuntime js;
-
-//     public JsInteropClasses1(IJSRuntime js)
-//     {
-//         this.js = js;
-//     }
-
-//     public async ValueTask TickerChanged(string symbol, decimal price)
-//     {
-//         await js.InvokeVoidAsync("displayTickerAlert1", symbol, price);
-//     }
-
-//     public void Dispose()
-//     {
-//     }
-// }
-        // public ActionResult CreateMtng(MeetingVM objVm)
-        // {
-        //     Data access layer object    
-        //     DAL objDal = newDAL();    
-
-        //     //meeting master object created     
-        //     MeetingMaster mtngMasterDetails = newMeetingMaster();    
-
-        //     mtngMasterDetails.Name = objVm.mtngNameCovered.MtngName.ToString();    
-        //     mtngMasterDetails.Agenda_Covered = objVm.agendaCovered.AgendaName.ToString().Trim();    
-        //     mtngMasterDetails.Date_Of_Meeting = objVm.mtngDate.DateOfMeeting.ToString().Trim();    
-        //     mtngMasterDetails.End_Time = objVm.mtngDate.EndTime.ToString().Trim();    
-        //     mtngMasterDetails.Start_Time = objVm.mtngDate.StartTime.ToString().Trim();     
-        //     //added to the in memory    
-        //     objDal.mtngMaster.Add(mtngMasterDetails);    
-
-        //     //saved to db    
-        //     objDal.SaveChanges();       
-        // }
+            Console.WriteLine("Base64 Id is " + newBachelorette.Base64); 
+            string text = System.IO.File.ReadAllText(@"C:\Users\salimvay\OneDrive - Intel Corporation\Desktop\base64.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            return RedirectToAction("Create");            
+        } 
     }
 }
--- BacheloretteClient/Startup.cs
+++ BacheloretteClient/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace BacheloretteClient
 {
     public class Startup
@@ -24,6 +25,7 @@ namespace BacheloretteClient
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // services.AddBlazoredLocalStorage();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
